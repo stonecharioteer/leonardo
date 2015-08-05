af5e08c8ddf668f74a1cf9bdada856fe0ccb7133
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2015-08-05 21:51:22 +0530

    Initial commits. Still setting things up.

--- LeonardoMethods/ShellShocked.py
+++ LeonardoMethods/ShellShocked.py
@@ -1,5 +1,17 @@
-from PyQt4 import QtGui
+import os
+from PyQt4 import QtGui, QtCore
+
 def showSplashScreen(min_time=None):
 	if min_time is None:
 		min_time = 3
+	image = os.path.join("essentials","half-shell.png")
+    splash_pix = QtGui.QPixmap(image)
+    splash = QtGui.QSplashScreen(splash_pix, QtCore.Qt.WindowStaysOnTopHint)
+    splash.setMask(splash_pix.mask())
+    splash.show()
+    time.sleep(min_time)
+    return splash
+
+def setWindowTheme()
+	QtGui.qApp.setStyle(QtCore.QString(u'Plastique'))
 
