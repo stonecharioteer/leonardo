329d55fc3504dc124acc6cbf7e8d655194c8c20b
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2015-09-28 16:05:56 +0530

    Fixing layout.

--- Sewers/Katana.py
+++ Sewers/Katana.py
@@ -414,9 +414,9 @@ def getIconsAndCoordinates(base_image, parent_image_size, parent_image_coords, p
                             ]
                 elif icons_required == 5:
                     icon_positions =  [
-                            (canvas_x_left+0.05*max_icon_width, canvas_y_top),
+                            (canvas_x_left+1.5*max_icon_width, canvas_y_top),
                             (canvas_center_x, canvas_y_top),
-                            (canvas_x_right-0.05*max_icon_width, canvas_y_top),
+                            (canvas_x_right-1.5*max_icon_width, canvas_y_top),
                             (canvas_x_left+max_icon_width/2, canvas_y_bottom), 
                             (canvas_x_right-max_icon_width/2, canvas_y_bottom)
                             ]
@@ -435,9 +435,9 @@ def getIconsAndCoordinates(base_image, parent_image_size, parent_image_coords, p
                             (canvas_center_x-0.5*max_icon_width, canvas_y_top),
                             (canvas_center_x+0.5*max_icon_width, canvas_y_top),
                             (canvas_x_right, canvas_y_top),
-                            (canvas_x_left, canvas_y_bottom),
+                            (canvas_center_x-1.5*max_icon_width, canvas_y_bottom),
                             (canvas_center_x, canvas_y_bottom),
-                            (canvas_x_right, canvas_y_bottom)
+                            (canvas_center_x+1.5*max_icon_width, canvas_y_bottom)
                     ]
                 else:
                     raise Exception("I  haven't accounted for %d icons"%icons_required)                
