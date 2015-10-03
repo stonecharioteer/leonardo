d05244af8dfeeae3c9d5c6035c1d38809cb6d27f
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2015-10-03 07:59:00 +0530

    Continuing class work.

--- Sewers/USPImage.py
+++ Sewers/USPImage.py
@@ -1,6 +1,17 @@
+from __future__ import division
+import math
+import os
+import datetime
+import glob
+import random
+import numpy as np
+import pandas as pd
 from PIL import Image, ImageFilter, ImageQt
 import PIL
 from PyQt4.QtGui import QImage
+
+import Katana
+
 
 class USPImage:
     def __init__(self, fsn, product_image_path, icon_data, background_image_path, brand_name, output_path, **kwargs):
@@ -10,14 +21,16 @@ class USPImage:
         self.fsn = fsn
         self.product_image_path = product_image_path
         self.icon_data = icon_data
-        self.background_image_path = background_image_path
-        self.brand_name = brand_name
         self.output_path = output_path
         aspect_ratio = [9,14]
         image_width = 1800
         resize_reference = 0
         resize_factor = 0.5
-        color_strip_algorithm = 0        
+        color_strip_algorithm = 0
+        icon_colors = None
+        icon_text_color = [0, 0, 0]
+        icon_shape = None
+
         if kwargs is not none:
             if "aspect_ratio" in kwargs.keys():
                 aspect_ratio = kwargs["aspect_ratio"]
@@ -29,22 +42,33 @@ class USPImage:
                 resize_factor = kwargs["resize_factor"]
             if "color_strip_algorithm" in kwargs.keys():
                 color_strip_algorithm = kwargs["color_strip_algorithm"]
+            if "icon_colors" in kwargs.keys():
+                icon_colors = kwargs["icon_colors"]
+            if "icon_text_color" in kwargs.keys():
+                icon_text_color = kwargs["icon_text_color"]
+            if "icon_shape" in kwargs.keys():
+                icon_shape = kwargs["icon_shape"]
 
         self.prepareCanvas(aspect_ratio, image_width)
-        self.loadBackgroundImage()
+        self.loadBackgroundImage(background_image_path)
         self.prepareProductImage(resize_reference, resize_factor, color_strip_algorithm)
         self.prepareIcons()
+        self.prepareFSNBrandLogo(brand_name)
         self.calculatePositions()
         if not self.detectOverlaps(): self.applyLayout()
+        self.saveImage()
 
     def prepareCanvas(self, aspect_ratio, image_width):
         """Prepares a canvas for plotting the image. 
         This canvas's size is the limiting factor for the entire image."""
-        pass
-    
+        image_height = math.ceil(image_width*aspect_ratio[1]/aspect_ratio[0])
+        canvas_size = (int(image_width), int(image_height))
+        self.canvas = Image.new("RGBA", canvas_size, (255,255,255,255))
+        
     def loadBackgroundImage(self):
         """Opens the background image and resizes it to suit the size of the canvas."""
-        pass
+        canvas_size = self.canvas.size
+
 
     def prepareProductImage(self, resize_reference, resize_factor, color_strip_algorithm, **kwargs):
         """
@@ -77,12 +101,16 @@ class USPImage:
         Returns False if there's none.
         """
         pass
+
     def applyLayout(self):
         pass
+
     def tryLayout(self):
         pass
+
     def saveImage(self):
         pass
+
     def dumpLayers(self):
         for layer in self.layers:
             layer_name = layer["Name"]
@@ -94,11 +122,9 @@ class USPImage:
         Returns the USP Image as a PIL.Image.
         """
         return self.usp_image
+
     def getQtImage(self):
         """
         Returns the USP Image as a PyQt4.QtGui.QImage.
         """
         return ImageQt.ImageQt(self.getPILImage())
-
-
-
